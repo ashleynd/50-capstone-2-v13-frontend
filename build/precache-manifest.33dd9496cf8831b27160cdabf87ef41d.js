7cfaf3e04e2367e11fba1f7409ea1948b2a85f43
Ashley Dragan <ashleyndragan@gmail.com>
2021-11-08 22:30:27 -0500

    api slash

---
+++ build/precache-manifest.33dd9496cf8831b27160cdabf87ef41d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e0de59dcefa1be23cd0ffd31c23d6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b931c4e6db989c74d60",
+    "url": "/static/css/2.bceb405c.chunk.css"
+  },
+  {
+    "revision": "0de9cf353b059834f84a",
+    "url": "/static/css/main.9ef1baeb.chunk.css"
+  },
+  {
+    "revision": "0b931c4e6db989c74d60",
+    "url": "/static/js/2.7aa101c5.chunk.js"
+  },
+  {
+    "revision": "1be221c4b76e0f38086eb8ad4eb03885",
+    "url": "/static/js/2.7aa101c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de9cf353b059834f84a",
+    "url": "/static/js/main.31128d8f.chunk.js"
+  },
+  {
+    "revision": "86e4250c3da6f7987b35",
+    "url": "/static/js/runtime-main.89f21953.js"
+  },
+  {
+    "revision": "d13fce44dce43293935c2bf5e38b33ab",
+    "url": "/static/media/headshot.d13fce44.png"
+  },
+  {
+    "revision": "8d2cd246fea23f46bc746011a552f825",
+    "url": "/static/media/spinner.8d2cd246.gif"
+  }
+]);
