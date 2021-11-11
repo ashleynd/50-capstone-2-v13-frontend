91e889c7ab6b1d2e89f68da96522573ecd931a1d
Ashley Dragan <ashleyndragan@gmail.com>
2021-11-10 19:48:36 -0500

    added in one test

--- src/routes-nav/Navigation.test.js
+++ src/routes-nav/Navigation.test.js
@@ -14,24 +14,3 @@ it("renders without crashing", function () {
   );
 });
 
-it("matches snapshot", function () {
-  const { asFragment } = render(
-      <MemoryRouter>
-        <UserProvider>
-          <Navigation />
-        </UserProvider>
-      </MemoryRouter>,
-  );
-  expect(asFragment()).toMatchSnapshot();
-});
-
-it("matches snapshot when logged out", function () {
-  const { asFragment } = render(
-      <MemoryRouter>
-        <UserProvider currentUser={null}>
-          <Navigation />
-        </UserProvider>
-      </MemoryRouter>,
-  );
-  expect(asFragment()).toMatchSnapshot();
-});
